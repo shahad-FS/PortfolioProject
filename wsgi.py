04a311103e52ff172ab6014b0130a0329dbd2871
lina_aziz <lenaexo0@gmail.com>
2026-05-18 23:30:33 +0300

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-"""إعداد WSGI لمشروع Raouf."""
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "raouf.settings")
-
-application = get_wsgi_application()
