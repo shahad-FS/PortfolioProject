10ac0d1832b716d54427b7b30d5808fa71853b24
shahad-FS <alsaneea.s@gmail.com>
2026-06-13 06:25:55 +0300

    fix: mismatch url

--- rauf/backend/config/urls.py
+++ rauf/backend/config/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path("api/v1/medical/", include("medical.urls")),
     path("api/v1/video_sessions/", include("video_sessions.urls")),
     path("api/v1/payments/", include("payments.urls")),
-    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    path('api/v1/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
     # schema
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
 
